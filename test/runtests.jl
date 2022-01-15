b2e37fb181ae95e383792a2c55d9eb3df74da641
MarkNahabedian <naha@mit.edu>
2022-01-15 12:21:17 -0500

    Use method rather than slot reference.

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,7 +11,7 @@ using Logging
     @test keys(g) == Set([:a, :a1, :a2])
     @test values(g) == Set([:a1, :a2, :b])
     @test nodes(g) == Set([:a, :b, :a1, :a2])
-    @test query(g, Symbol, Symbol) == g.edges
+    @test query(g, Symbol, Symbol) == edges(g)
     @test query(g, :a, Any) == Set([:a => :a1, :a => :a2])
     @test query(g, Any, :b) == Set([:a1 => :b, :a2 => :b])
     remove_edge!(g, :a2 => :b)
